83343b791a75f3883fc01fec50834902900c28ea
masterfloflo <43879013+masterfloflo@users.noreply.github.com>
2018-12-05 12:01:02 +0100

    a

--- src/main/java/dev/top/controller/CollegueCtrl.java
+++ src/main/java/dev/top/controller/CollegueCtrl.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +32,7 @@ public class CollegueCtrl {
 	}
 	*/
 	
-	@RequestMapping("{/pseudo}")
+	@PatchMapping("{/pseudo}")
     public Collegue patch(@PathVariable String pseudo, Map<String, String> vote) {
 
         String resultat = vote.get("actions");
